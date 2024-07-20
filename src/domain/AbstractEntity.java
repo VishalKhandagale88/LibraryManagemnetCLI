5dc41bef2a37a11705b78010cf0ad513f6dfb618
vishal <vishal88kh@gmail.com>
2024-07-20 11:03:01 +0530

    created AbstractEntity

--- src/domain/AbstractEntity.java
+++ src/domain/AbstractEntity.java
@@ -1,2 +1,23 @@
-package domain;public class AbstractEntity {
+package domain;
+
+public abstract class AbstractEntity {
+    // 1st object
+    // 0.0 1
+    // 2nd object
+    // 0.0 2
+    // 3rd object
+    // 0.0 3
+    private static long idCounter = 0;
+    private final long id;  // 0.0
+
+    public AbstractEntity() {
+        this.id = idCounter++;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public abstract void display();
+
 }
